66cf3aeb6ac65873485db8920e3d5120ee693149
Ismoil <samandarmurodov2003@gmail.com>
2024-11-21 20:53:12 +0500

    Rus tilida registratsiya ham togirlandi

--- handlers/users.py
+++ handlers/users.py
@@ -4,7 +4,7 @@ from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 
-from keyboards import language_kb, phone_kb, skip_kb
+from keyboards import language_kb, phone_kb, skip_kb,phone_kb_ru,skip_kb_ru
 from middlewares import LanguageMiddleware
 
 from aiogram import Router
@@ -29,63 +29,97 @@ class Registration(StatesGroup):
 # @router.message(CommandStart("/start"))
 @router.message(Command(commands=["start"]))
 async def start_command(message: Message, state: FSMContext):
-    await message.answer("Salom! Tilni tanlang:", reply_markup=language_kb)
+    await message.answer(
+        "Выберите язык:\n\n🇺🇿 O'zbek\n🇷🇺 Русский",
+        reply_markup=language_kb
+    )
 
 
-# Tilni tanlash
 @router.message(lambda msg: msg.text in languages)
 async def choose_language(message: Message, state: FSMContext):
     lang = languages[message.text]
     LanguageMiddleware.set_language(message.from_user.id, lang)
-    await message.answer("Ismingizni kiriting:")
+
+    if lang == "uz":
+        await message.answer("Ismingizni kiriting:")
+    elif lang == "ru":
+        await message.answer("Введите ваше имя:")
+
     await state.set_state(Registration.name)
 
 
-# Ismni so'rash
 @router.message(Registration.name)
 async def ask_name(message: Message, state: FSMContext):
+    lang = LanguageMiddleware.get_language(message.from_user.id)
+
     await state.update_data(name=message.text)
-    await message.answer("Telefon raqamingizni ulashing yoki qo'lda kiriting:", reply_markup=phone_kb)
+    if lang == "uz":
+        await message.answer("Telefon raqamingizni ulashing yoki qo'lda kiriting:", reply_markup=phone_kb)
+    elif lang == "ru":
+        await message.answer("Поделитесь своим номером телефона или введите его вручную:", reply_markup=phone_kb_ru)
+
     await state.set_state(Registration.contact_number)
 
 
 @router.message(Registration.contact_number)
 async def ask_phone(message: Message, state: FSMContext):
-    # Agar foydalanuvchi tugma orqali ulashsa
+    lang = LanguageMiddleware.get_language(message.from_user.id)
+
     if message.contact:
         contact_number = message.contact.phone_number
     else:
         contact_number = message.text
 
-    # Telefon raqamni tekshirish
     if contact_number.startswith("+998") and len(contact_number[4:]) == 9 and contact_number[4:].isdigit():
         await state.update_data(contact_number=contact_number)
-        await message.answer("Tug'ilgan sanangizni kiriting yoki o'tkazib yuboring:", reply_markup=skip_kb)
+        if lang == "uz":
+            await message.answer("Tug'ilgan sanangizni kiriting yoki o'tkazib yuboring:", reply_markup=skip_kb)
+        elif lang == "ru":
+            await message.answer("Введите вашу дату рождения или пропустите:", reply_markup=skip_kb_ru)
         await state.set_state(Registration.birthday)
     else:
-        await message.answer(
-            "Telefon raqam noto'g'ri formatda! "
-            "Raqamni quyidagi formatda kiriting: +998901234567"
-        )
+        if lang == "uz":
+            await message.answer(
+                "Telefon raqam noto'g'ri formatda! "
+                "Raqamni quyidagi formatda kiriting: +998901234567"
+            )
+        elif lang == "ru":
+            await message.answer(
+                "Номер телефона в неправильном формате! "
+                "Введите номер в следующем формате: +998901234567"
+            )
 
 
-# Tug'ilgan sanani so'rash
 @router.message(Registration.birthday)
 async def ask_birthdate(message: Message, state: FSMContext):
-    if message.text.lower() == "❌ o'tkazib yuborish":
+    lang = LanguageMiddleware.get_language(message.from_user.id)
+
+    if message.text.lower() in ["❌ o'tkazib yuborish", "❌ пропустить"]:
         birthday = None
     else:
         birthday = message.text
 
-    await state.update_data(birthday=birthday)
+    await state.update_data(birthdate=birthday)
 
-    # Malumotlarni olish
     data = await state.get_data()
     data["tg_id"] = message.from_user.id
-    await message.answer(f"Ro'yxatdan o'tdingiz!\n\n"
-                         f"Ism: {data['name']}\n"
-                         f"Telefon: {data['contact_number']}\n"
-                         f"Tug'ilgan sana: {data.get('birthday', "Ko'rsatilmagan")}")
 
+    if lang == "uz":
+        success_message = (
+            f"Ro'yxatdan o'tdingiz!\n\n"
+            f"Ism: {data['name']}\n"
+            f"Telefon: {data['contact_number']}\n"
+            f"Tug'ilgan sana: {data.get('birthday', "Ko'rsatilmagan")}"
+        )
+    elif lang == "ru":
+        success_message = (
+            f"Вы зарегистрированы!\n\n"
+            f"Имя: {data['name']}\n"
+            f"Телефон: {data['contact_number']}\n"
+            f"Дата рождения: {data.get('birthday', 'Не указано')}"
+        )
+
+    await message.answer(success_message)
     await state.clear()
-    print(data)
+    print(data)  # Admin uchun
+
--- keyboards.py
+++ keyboards.py
@@ -1,17 +1,27 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton,ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 
 # Tilni tanlash uchun klaviatura
 language_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="🇺🇿 O'zbek")],
     [KeyboardButton(text="🇷🇺 Русский")]
-], resize_keyboard=True,ReplyKeyboardRemove=True)
+], resize_keyboard=True, ReplyKeyboardRemove=True)
 
 # Telefon raqamini ulashish uchun klaviatura
 phone_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="📱 Telefon raqamni ulashish", request_contact=True)]
 ], resize_keyboard=True, one_time_keyboard=True)
 
+# Telefon raqamini ulashish uchun klaviatura(ru)
+phone_kb_ru = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="📱 Поделитесь своим номером телефона", request_contact=True)]
+], resize_keyboard=True, one_time_keyboard=True)
+
 # O'tkazib yuborish tugmasi uchun klaviatura
 skip_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="❌ O'tkazib yuborish")]
 ], resize_keyboard=True)
+
+# O'tkazib yuborish tugmasi uchun klaviatura (ru)
+skip_kb_ru = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="❌ пропустить")]
+],resize_keyboard=True)
